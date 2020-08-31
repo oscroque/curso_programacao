40b1e593f005b6aa32fead24253e7fde4b2b9d44
Oscar Roque <oscroque@gmail.com>
2020-08-30 21:30:10 -0300

    Projeto criado

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Locale;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Olá Mundo");
+		double x = 12.1234;
+		Locale.setDefault(Locale.US);
+		System.out.printf("%.2f%n", x);
+		
+	}
+
+}
